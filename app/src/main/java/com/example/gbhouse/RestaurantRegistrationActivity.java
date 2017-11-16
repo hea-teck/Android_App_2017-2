053fb844bd3045c9574796fcb45b8bd47d5fb46e
hea-teck <31760928+hea-teck@users.noreply.github.com>
2017-11-17 00:50:59 +0900

    RestaurantRegistrationActivity 수정
    이미지 버튼 클릭시 카메라 앱이 실행되며 촬영된 사진을 저장 후 다시 이미지 버튼에 보이도록 설정

--- app/src/main/java/com/example/gbhouse/RestaurantRegistrationActivity.java
+++ app/src/main/java/com/example/gbhouse/RestaurantRegistrationActivity.java
@@ -27,9 +27,51 @@ public class RestaurantRegistrationActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                dispatchTakePictureIntent();
             }
         });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (mPhotoFileName != null) {
+                mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+
+                ImageButton imageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+                imageButton3.setImageURI(Uri.fromFile(mPhotoFile));
+            }
+        }
+    }
+
+    String mPhotoFileName;
+    File mPhotoFile;
+
+    final int REQUEST_IMAGE_CAPTURE = 100;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //1. 카메라 앱으로 찍은 이미지를 저장할 파일 객체 생성
+            mPhotoFileName = "IMG" + currentDateFormat() + ".jpg";
+            mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+
+            if (mPhotoFile != null) {
+                //2. 생성된 파일 객체에 대한 Uri 객체를 얻기
+                Uri imageUri = FileProvider.getUriForFile(this, "com.example.gbhouse", mPhotoFile);
+
+                //3. Uri 객체를 Extras를 통해 카메라 앱으로 전달
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private String currentDateFormat() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+
 }
 
