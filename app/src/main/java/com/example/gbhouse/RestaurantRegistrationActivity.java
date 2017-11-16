b6d9e011666c257cd28dba58c4862e1dd659a988
hea-teck <31760928+hea-teck@users.noreply.github.com>
2017-11-16 17:31:47 +0900

    RestaurantRegistrationActivity 수정
    RestaurantRegistrationActivity 에서 이미지 버튼을 클릭 시 이벤트 생성 추가

--- app/src/main/java/com/example/gbhouse/RestaurantRegistrationActivity.java
+++ app/src/main/java/com/example/gbhouse/RestaurantRegistrationActivity.java
@@ -1,7 +1,18 @@
 package com.example.gbhouse;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by 김희택 on 2017-11-16.
@@ -11,5 +22,14 @@ public class RestaurantRegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_registration);
+
+        ImageButton btn = (ImageButton) findViewById(R.id.imageButton3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
+
