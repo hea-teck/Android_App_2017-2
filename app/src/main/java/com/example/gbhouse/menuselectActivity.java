3c8388169b9492d380441556ebde189c8308d160
saeugeen <dbwls7083@naver.com>
2017-11-18 13:15:48 +0900

    메뉴 등록
    메뉴 등록 (menuselectActivity)

---
+++ app/src/main/java/com/example/gbhouse/menuselectActivity.java
@@ -0,0 +1,15 @@
+package com.example.gbhouse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by B10542 on 2017-11-17.
+ */
+
+public class menuselectActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_select);
+    }
+}
